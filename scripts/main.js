5719ffa0452754638864a4cdea049a4c6b093375
Denys Dovhan <email@denysdovhan.com>
2017-04-05 07:45:33 +0000

    Update Wed Apr 05 2017 07:45:31 GMT+0000 (UTC)

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mapboxURL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+var venue = L.map('venue', {
+  scrollWheelZoom: false
+}).setView(coords, 16);
+
+L.tileLayer(mapboxURL, {
+  maxZoom: 18,
+  id: 'mapbox.streets'
+}).addTo(venue);
+
+L.marker(coords).addTo(venue).openPopup();
