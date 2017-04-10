2067bdce682ac91bef13ed821a863ab8295354d4
Denys Dovhan <email@denysdovhan.com>
2017-04-10 18:21:02 +0300

    Update date

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const stylus = require('gulp-stylus');
-const postcss = require('gulp-postcss');
-const babel = require('gulp-babel');
-const pug = require('gulp-pug');
-const put = require('gulp-data');
-const imagemin = require('gulp-imagemin');
-const deploy = require('gulp-gh-pages');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp')
+const plumber = require('gulp-plumber')
+const stylus = require('gulp-stylus')
+const postcss = require('gulp-postcss')
+const babel = require('gulp-babel')
+const pug = require('gulp-pug')
+const put = require('gulp-data')
+const imagemin = require('gulp-imagemin')
+const deploy = require('gulp-gh-pages')
+const sourcemaps = require('gulp-sourcemaps')
 
-const JSON5 = require('json5');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const del = require('del');
-const BS = require('browser-sync');
+const JSON5 = require('json5')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const del = require('del')
+const BS = require('browser-sync')
 
-const browserSync = BS.create();
+const browserSync = BS.create()
 
 const getData = (path) => {
   try {
     return JSON5.parse(fs.readFileSync(path, 'utf8'))
   } catch (e) {
-    console.error(e);
+    console.error(e) // eslint-disable-line
   }
-};
+}
 
 gulp.task('layout', () =>
   gulp.src('*.pug')
@@ -33,13 +33,13 @@ gulp.task('layout', () =>
     .pipe(put(() => getData('./data.json')))
     .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('dist'))
-);
+)
 
 gulp.task('styles', () => {
   const processors = [
     autoprefixer,
     cssnano,
-  ];
+  ]
 
   return gulp.src('styles/*.styl')
     .pipe(sourcemaps.init())
@@ -48,7 +48,7 @@ gulp.task('styles', () => {
     .pipe(postcss(processors))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/styles'))
-});
+})
 
 gulp.task('scripts', () =>
   gulp.src('scripts/*.js')
@@ -60,15 +60,15 @@ gulp.task('images', () =>
   gulp.src('images/**/*.*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
-);
+)
 
 gulp.task('copy', () =>
   gulp.src([
     'CNAME', 'favicon*', './mail/*.*', 'join/*.*'], { base: '.' })
     .pipe(gulp.dest('dist'))
-);
+)
 
-gulp.task('clean', () => del('dist'));
+gulp.task('clean', () => del('dist'))
 
 gulp.task('build', gulp.series(
   'clean',
@@ -79,7 +79,7 @@ gulp.task('build', gulp.series(
     'images',
     'copy'
   )
-));
+))
 
 gulp.task('deploy', gulp.series('build', () =>
   gulp.src('dist/**/*')
@@ -88,19 +88,19 @@ gulp.task('deploy', gulp.series('build', () =>
       push: true,
       message: `Update ${new Date()}`
     }))
-));
+))
 
 gulp.task('watch', () => {
   browserSync.init({
     server: 'dist',
-  });
+  })
 
-  gulp.watch(['*.pug', '*.json'], gulp.series('layout'));
-  gulp.watch('styles/*.styl', gulp.series('styles'));
-  gulp.watch('scripts/*.js', gulp.series('scripts'));
-  gulp.watch('images/**/*.*', gulp.series('images'));
+  gulp.watch(['*.pug', '*.json'], gulp.series('layout'))
+  gulp.watch('styles/*.styl', gulp.series('styles'))
+  gulp.watch('scripts/*.js', gulp.series('scripts'))
+  gulp.watch('images/**/*.*', gulp.series('images'))
 
-  browserSync.watch('dist/**/*.*').on('change', browserSync.reload);
-});
+  browserSync.watch('dist/**/*.*').on('change', browserSync.reload)
+})
 
-gulp.task('default', gulp.series('build', 'watch'));
+gulp.task('default', gulp.series('build', 'watch'))
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,20 @@
+/* eslint no-undef: 0 */
 const mapboxURL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 
 const map = L
   .map('embedded-map', {
     scrollWheelZoom: false,
   })
-  .setView(coords, 16);
+  .setView(coords, 16)
 
 L
   .tileLayer(mapboxURL, {
     maxZoom: 18,
     id: 'mapbox.streets',
   })
-  .addTo(map);
+  .addTo(map)
 
 L
   .marker(coords)
   .addTo(map)
-  .openPopup();
+  .openPopup()
