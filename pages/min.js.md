08df53e10a4e4b823bebfc0b89e2f9cf47e1b7b1
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2018-11-10 22:30:49 +0200

    add page for meetup

---
+++ pages/min.js.md
@@ -0,0 +1,7 @@
+---
+layout: layout/min.pug
+title: Meetup 01.12.2018
+---
+
+The chernivtsi.min.js is a ChernivtsiJS meetup that will take place in Chernivtsi on December 1.
+
--- scripts/index.js
+++ scripts/index.js
@@ -6,26 +6,51 @@ import config from '../data.yml'
  */
 
 const { latitude, longitude } = config.place.coords
+const { latitude: latitudeMin, longitude: longitudeMin } = config.placeMin.coords
+if(document.body.querySelector('#embedded-map')) {
+    const map = L
+    .map('embedded-map', {
+        scrollWheelZoom: false,
+        dragging: !L.Browser.mobile,
+        tap: false
+    })
+    .setView([latitude, longitude], 16)
 
-const map = L
-  .map('embedded-map', {
-    scrollWheelZoom: false,
-    dragging: !L.Browser.mobile,
-    tap: false
-  })
-  .setView([latitude, longitude], 16)
+    L
+    .tileLayer(config.place.url, {
+        maxZoom: 18,
+        id: 'mapbox.streets',
+    })
+    .addTo(map)
 
-L
-  .tileLayer(config.place.url, {
-    maxZoom: 18,
-    id: 'mapbox.streets',
-  })
-  .addTo(map)
+    L
+    .marker([latitude, longitude])
+    .addTo(map)
+    .openPopup()
+}
 
-L
-  .marker([latitude, longitude])
-  .addTo(map)
-  .openPopup()
+
+if (document.body.querySelector('#embedded-map-min')) {
+    const mapMin = L
+    .map('embedded-map-min', {
+        scrollWheelZoom: false,
+        dragging: !L.Browser.mobile,
+        tap: false
+    })
+    .setView([latitudeMin, longitudeMin], 16)
+
+    L
+    .tileLayer(config.placeMin.url, {
+        maxZoom: 20,
+        id: 'mapbox.streets',
+    })
+    .addTo(mapMin)
+
+    L
+    .marker([latitudeMin, longitudeMin])
+    .addTo(mapMin)
+    .openPopup()
+}
 
 /**
  * Tweets from last year
@@ -33,7 +58,7 @@ L
 
 if (config.tweets && config.tweets.length > 0) {
   const tweetsContainer = document.getElementById('tweets-container')
-  
+
   config.tweets.forEach(tweetId => twttr.widgets.createTweet(
     tweetId,
     tweetsContainer,
